79931be4a1452555bc7476dcc13660f830ff372b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-25 16:11:34 -0600

    Added loading gif

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,8 +92,14 @@ $(document).on('page:change', function() {
 	}
 	if (newHTML != oldHTML) {
 		$(".content").html(oldHTML)
+		if (mobile){
+			$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).append("&nbsp;&nbsp;<img class='loading' height='10' src='/assets/loading.gif' />")
+		} else {
+			$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).parent().append("<img class='loading' height='10' src='/assets/loading.gif' />")
+		}
 		$("nav ul").promise().done(function(){
 			$(".content").fadeOut(250, function(){
+				$(".loading").remove()
 				$(".content").html(newHTML)
 				$(".content").fadeIn(250)
 			});
