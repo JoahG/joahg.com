d3305cf2debb7c7fc4c0f19304d09eb01314779d
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-26 08:20:07 -0600

    Movin lotsa files over...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	
+	after_filter :set_access_control_headers
+	rescue_from Exception, :with => :error_method
+
+	def set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Request-Method'] = '*'
+	end
+
+	def error_method
+		respond_to do |format|
+		    format.html { render :template => "p/fourofour" }
+		end
+	end
 end
---
+++ app/controllers/p_controller.rb
@@ -0,0 +1,16 @@
+class PController < ApplicationController
+	def index
+	end
+
+	def find
+	end
+
+	def about
+	end
+
+	def portfolio
+	end
+
+	def fourofour
+	end
+end
