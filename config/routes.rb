3317a1fc8cf3c675673cd355e8982b0aabd511bc
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-26 08:58:12 -0600

    Added back g+ shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Joahg::Application.routes.draw do
+  get "+" => redirect("https://plus.google.com/+JoahGerstenberg"), :format => false
   get "portfolio" => "p#portfolio", :as => "portfolio"
   get "about" => "p#about", :as => "about"
   get "find" => "p#find", :as => "find"
