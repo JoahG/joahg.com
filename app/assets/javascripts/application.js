ce35bf0f263949c55022cf2eb1c7746ab1c281bd
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-01 15:01:10 -0600

    Removed buggy loader gif

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -85,11 +85,11 @@ $(document).on('page:fetch', function() {
 
 $(document).on('page:change', function() {
 	newHTML = $(".content").html()
-	if (mobile){
-		$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).append("&nbsp;&nbsp;<img class='loading' height='10' src='/assets/loading.gif' />")
-	} else {
-		$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).parent().append("<img class='loading' height='10' src='/assets/loading.gif' />")
-	}
+	// if (mobile){
+	// 	$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).append("&nbsp;&nbsp;<img class='loading' height='10' src='/assets/loading.gif' />")
+	// } else {
+	// 	$(".nav_"+(window.location.href.split("/")[window.location.href.split("/").length-1].length > 0 ? window.location.href.split("/")[window.location.href.split("/").length-1] : "home")).parent().append("<img class='loading' height='10' src='/assets/loading.gif' />")
+	// }
 	if (mobile && mobileNavShown) {
 		$("nav ul").show()
 		$("nav ul").hide("blind", {direction: "vertical"}, 300)
@@ -99,7 +99,7 @@ $(document).on('page:change', function() {
 		$(".content").html(oldHTML)
 		$("nav ul").promise().done(function(){
 			$(".content").fadeOut(250, function(){
-				$(".loading").remove()
+				// $(".loading").remove()
 				$(".content").html(newHTML)
 				$(".content").fadeIn(250)
 			});
